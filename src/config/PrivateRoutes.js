5f7368207f2e780ea14061a12449afb310b47fa4
Nguyễn Trung Hiếu <73569473+nthieu1332002@users.noreply.github.com>
2023-02-08 10:47:28 +0700

    fix bug

--- src/config/PrivateRoutes.js
+++ src/config/PrivateRoutes.js
@@ -1,11 +1,10 @@
 import { Outlet, Navigate } from "react-router-dom"
 import Cookies from 'js-cookie'
-import { toast } from "react-toastify"
 
 const PrivateRoutes = () => {
     let auth = Cookies.get("token")
     if (auth === undefined)  {
-        toast.error("You do not have permission to access this.")
+        // toast.error("You do not have permission to access this.")
     }
     return (auth ? <Outlet /> : <Navigate to="/login" />)
 }
